7d8848ac0cc25e7eb35d1baed4c7642397eae5d8
gilang <gilangsos99@gmail.com>
2022-07-04 09:39:15 +0700

    perubahan kode

--- asset/javascript/index.js
+++ asset/javascript/index.js
@@ -1,17 +1,17 @@
-// let currentSlide = 1;
-// let $slider = $(".slides");
-// let slideCount = $slider.children().length;
-// let slideTime = 3000;
-// let animationTime = 800;
-// setInterval(function(){
-//   $slider.animate({
-//       marginLeft : '-=100%'
-//   } , animationTime , function() {
-//     currentSlide++;
-//     if(currentSlide === slideCount){
-//       currentSlide = 1;
-//       $this.css("margin-left" , "0px")
-//     }
-//   })
-// }, slideTime)
+let currentSlide = 1;
+let $slider = $(".slides");
+let slideCount = $slider.children().length;
+let slideTime = 3000;
+let animationTime = 800;
+setInterval(function(){
+  $slider.animate({
+      marginLeft : '-=100%'
+  } , animationTime , function() {
+    currentSlide++;
+    if(currentSlide === slideCount){
+      currentSlide = 1;
+      $this.css("margin-left" , "0px")
+    }
+  })
+}, slideTime)
 
